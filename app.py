70ffea485bae4c6e13bdfd50c3441bf4ce311d85
Dmitriy Korotkov <nupellot@gmail.com>
2022-12-16 05:39:19 +0300

    Залил основной обработчик проекта

---
+++ app.py
@@ -0,0 +1,51 @@
+import json
+
+from flask import Flask, render_template, session, request, redirect, url_for
+from auth.routes import blueprint_auth
+from catalog.routes import blueprint_catalog
+from market.routes import blueprint_market
+from orderlist.routes import blueprint_orderlist
+from report.routes import blueprint_report
+from basket.routes import blueprint_basket
+from access import login_required
+from patient.routes import blueprint_patient
+
+app = Flask(__name__, template_folder='templates', static_folder="static")
+app.secret_key = 'SuperKey'
+
+app.register_blueprint(blueprint_auth, url_prefix='/auth')
+app.register_blueprint(blueprint_report, url_prefix='/report')
+app.register_blueprint(blueprint_market, url_prefix='/market')
+app.register_blueprint(blueprint_basket, url_prefix='/basket')
+app.register_blueprint(blueprint_catalog, url_prefix='/catalog')
+app.register_blueprint(blueprint_orderlist, url_prefix='/orderlist')
+app.register_blueprint(blueprint_patient, url_prefix='/patient')
+
+
+
+app.config['db_config'] = json.load(open('configs/db.json'))
+app.config['access_config'] = json.load(open('configs/access.json'))
+app.config['cache_config'] = json.load(open('configs/cache.json'))
+
+
+@app.route('/', methods=['GET', 'POST'])
+# @login_required
+def menu_choice():
+    return redirect(url_for("bp_patient.dashboard"))
+    # return render_template("base.html", session=session, request=request)
+    # if session.get('user_group', None):
+    #     return render_template('internal_user_menu.html', session=session, request=request)
+    # return render_template('external_user_menu.html')
+
+
+@app.route('/exit')
+@login_required
+def exit_func():
+    session.clear()
+    return render_template('exit.html')
+
+
+if __name__ == '__main__':
+    # app = add_blueprint_access_handler(app, ['blueprint_report'], group_required)
+    # app = add_blueprint_access_handler(app, ['blueprint_market'], external_required)
+    app.run(host='127.0.0.1', port=5001, debug=True)
