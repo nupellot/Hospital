c11e64fbed666ba320b8086039b37b6af28156b5
Dmitriy Korotkov <nupellot@gmail.com>
2023-02-11 01:50:26 +0300

    Расширил запрос о палатах информацией о докторе

--- ward/sql/ward.sql
+++ ward/sql/ward.sql
@@ -1,4 +1,4 @@
-SELECT *
-FROM department JOIN ward ON wards_department=id_department JOIN story ON story_ward=id_ward JOIN patient ON story_patient=id_patient
+SELECT department.*, ward.*, story.*, patient.*, doctor.name AS doctor_name, doctor.surname AS doctor_surname, doctor.patronymic AS doctor_patronymic
+FROM department JOIN ward ON wards_department=id_department JOIN story ON story_ward=id_ward JOIN patient ON story_patient=id_patient JOIN doctor ON id_doctor=story_doctor
 WHERE discharge_date IS NULL AND id_ward=$id_ward AND id_department=$id_department
 ORDER BY reg_date
