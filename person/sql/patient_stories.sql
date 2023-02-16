458ddde506429799e8344847eb79ecbfc886f078
Dmitriy Korotkov <nupellot@gmail.com>
2023-02-16 21:54:45 +0300

    Теперь в patient_stories.sql мы получаем больше информации

--- person/sql/patient_stories.sql
+++ person/sql/patient_stories.sql
@@ -1,3 +1,4 @@
-SELECT story.*
-FROM patient JOIN story ON story_patient=id_patient
-WHERE login="$login"
+SELECT story.*, doctor.image, doctor.surname, doctor.name, doctor.patronymic, doctor.login, id_ward, id_department
+FROM patient JOIN story ON story_patient=id_patient JOIN doctor ON story_doctor=id_doctor JOIN ward ON id_ward=story_ward JOIN department ON wards_department=id_department
+WHERE patient.login="$login"
+ORDER BY reg_date DESC
