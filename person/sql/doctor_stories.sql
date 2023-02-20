bb12848ce5c10b99aa3bb91dbf4129f7d6c4e7a6
Dmitriy Korotkov <nupellot@gmail.com>
2023-02-21 02:22:24 +0300

    Сделал так, чтобы акутальные истории были сверху

--- person/sql/doctor_stories.sql
+++ person/sql/doctor_stories.sql
@@ -7,3 +7,4 @@ union
     from survey join story on survey_story = id_story JOIN doctor ON story_doctor=id_doctor JOIN patient ON story_patient=id_patient JOIN ward ON id_ward=story_ward JOIN department ON wards_department=id_department
     WHERE survey_doctor = $doctor_id
 ) as temp
+ORDER BY discharge_date ASC
