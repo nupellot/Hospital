039e47678d494867a43c7056ed33b3e1f11365d4
Dmitriy Korotkov <nupellot@gmail.com>
2023-02-21 01:53:48 +0300

    + Запрос для получения информации обо всех историях доктора

---
+++ person/sql/doctor_stories.sql
@@ -0,0 +1,9 @@
+select distinct * from (
+    SELECT story.*, doctor.*, patient.image AS "patient_image", patient.name AS "patient_name", patient.surname AS "patient_surname", patient.patronymic AS "patient_patronymic", ward.*, department.*
+    from story JOIN doctor ON story_doctor=id_doctor JOIN patient ON story_patient=id_patient JOIN ward ON id_ward=story_ward JOIN department ON wards_department=id_department
+    WHERE story_doctor = $doctor_id
+union
+    SELECT story.*, doctor.*, patient.image AS "patient_image", patient.name AS "patient_name", patient.surname AS "patient_surname", patient.patronymic AS "patient_patronymic", ward.*, department.*
+    from survey join story on survey_story = id_story JOIN doctor ON story_doctor=id_doctor JOIN patient ON story_patient=id_patient JOIN ward ON id_ward=story_ward JOIN department ON wards_department=id_department
+    WHERE survey_doctor = $doctor_id
+) as temp
