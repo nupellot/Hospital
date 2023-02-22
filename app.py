8b662afde736035a42c0fa1eff4d4aabe4c53acd
Dmitriy Korotkov <nupellot@gmail.com>
2023-02-23 01:27:30 +0300

    Начал разработку Бекенда для дэшборда доктора

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask, render_template, session, request, redirect, url_for, current_app
 from auth.routes import blueprint_auth
 from catalog.routes import blueprint_catalog
+from database.connection import UseDatabase
 from database.operations import select_dict
 from database.sql_provider import SQLProvider
 from market.routes import blueprint_market
@@ -38,16 +39,43 @@ provider = SQLProvider(os.path.join(os.path.dirname(__file__), 'sql'))
 @app.route('/', methods=['GET', 'POST'])
 # @login_required
 def menu_choice():
-    if session["role"] == "doctor":
-        # Получаем информацию обо всех пациентах, которые сейчас лежат в госпитале.
-        sql_for_active_patients = provider.get("active_patients.sql")
-        # print("sql_for_active_patients:", sql_for_active_patients)
-        active_patients = select_dict(current_app.config['db_config'], sql_for_active_patients)
-        print("active_patients:", active_patients)
-        
-        return render_template('dashboard.html', patients=active_patients)
+    if request.method == "GET":
+        if session["role"] == "doctor":
+            # Получаем информацию обо всех пациентах, которые сейчас лежат в госпитале.
+            sql_for_active_patients = provider.get("active_patients.sql")
+            # print("sql_for_active_patients:", sql_for_active_patients)
+            active_patients = select_dict(current_app.config['db_config'], sql_for_active_patients)
+            print("active_patients:", active_patients)
+
+            return render_template('dashboard.html', patients=active_patients, session=session)
+        else:
+            return redirect(url_for("bp_person.person", user_login=session["login"]))
+    if request.method == "POST":
+        if session["role"] == "doctor":
+            print("request.form.get(\"id_patient\"):", request.form.get("id_patient"))
+            survey_patient = request.form.get("id_patient")
+            print("request.form.get(\"survey_text\"):", request.form.get("survey_text"))
+            prescriptions = request.form.get("survey_text")
+
+            sql_for_get_story_by_patient = provider.get("get_story_by_patient_id.sql", patient_id=survey_patient)
+            survey_story = select_dict(current_app.config['db_config'], sql_for_get_story_by_patient)
+            survey_story_id = survey_story[0]["id_story"]
+
+            sql_for_new_survey = provider.get("new_survey.sql",
+                                              prescriptions=prescriptions,
+                                              survey_doctor=session["id_doctor"],
+                                              survey_story=survey_story_id)
+
+
+            print("sql_for_new_survey:", sql_for_new_survey)
+
+            with UseDatabase(current_app.config['db_config']) as cursor:
+                cursor.execute(sql_for_new_survey)
+
+
+        return "KEk"
     else:
-        return redirect(url_for("bp_person.person", user_login=session["login"]))
+        return "Unknown request.method"
     # return render_template("base.html", session=session, request=request)
     # if session.get('user_group', None):
     #     return render_template('internal_user_menu.html', session=session, request=request)
